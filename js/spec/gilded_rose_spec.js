d66f3985b1b832dd455d43cd7a863d5597b12aab
Natalia Wyporek <if24b217@technikum-wien.at>
2025-06-14 20:01:03 +0200

    test: 'Conjured' items degrade in Quality twice as fast as normal items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -70,4 +70,11 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(0);
   });
 
+  it("'Conjured' items degrade in Quality twice as fast as normal items", function() {
+    items = [ new Item("Conjured Mana Cake", 3, 6) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(2);
+    expect(items[0].quality).toEqual(4);
+  });
+
 });
