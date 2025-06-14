98ea30bffb1bf2049c6c5f1c042b93791e3114b5
Natalia Wyporek <if24b217@technikum-wien.at>
2025-06-14 19:57:06 +0200

    test: Backstage passes quality drops to 0 after the concert

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -63,4 +63,11 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(40);
   });
 
+  it("Backstage passes quality drops to 0 after the concert", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
+  });
+
 });
