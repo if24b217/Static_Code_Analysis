eabf3daa7d53ad5461adab7dae4efb2ebba47c70
Natalia Wyporek <if24b217@technikum-wien.at>
2025-06-14 19:25:19 +0200

    test: degrade sell_in and quality of normal items by 1

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should degrade sell_in and quality by 1 for a normal item", function() {
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
 
 });
