a7350e4f90ea574569ac0f053b6e168fc95c731b
Natalia Wyporek <if24b217@technikum-wien.at>
2025-06-14 20:10:55 +0200

    feat: 'Conjured' items degrade in Quality twice as fast as normal items

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -16,9 +16,14 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+      if (items[i].quality > 0 && items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (items[i].name.includes("Conjured")) {
+          items[i].quality = items[i].quality - 2;
+        } else {
+          items[i].quality = items[i].quality - 1;
+        }
+        if (items[i].quality < 0) {
+          items[i].quality = 0;
         }
       }
     } else {
@@ -44,9 +49,14 @@ function update_quality() {
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+          if (items[i].quality > 0 && items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (items[i].name.includes("Conjured")) {
+              items[i].quality = items[i].quality - 2;
+            } else {
+              items[i].quality = items[i].quality - 1;
+            }
+            if (items[i].quality < 0) {
+              items[i].quality = 0;
             }
           }
         } else {
